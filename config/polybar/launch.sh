0e78454f1144de234702ea84ccd058d01b9df615
xncbot <xnc042@gmail.com>
2020-06-06 22:39:15 -0700

    adjust polybar and i3

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,11 +1,13 @@
+#!/usr/bin/env bash
+
 # Terminate already running bar instances
 killall -q polybar
 
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload example &
-    done
-else
-    polybar --reload example &
-fi
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
+done
+
+echo "Bars launched..."
