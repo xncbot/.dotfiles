1d87b543e9e680e514495966542f64116845c27d
xncbot <xnc042@gmail.com>
2020-06-27 17:47:27 -0700

    using pulseaudio-ctl and updates on polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,8 +8,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Launch bar1 and bar2
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
 done
 
-echo "Bars launched..."
+
